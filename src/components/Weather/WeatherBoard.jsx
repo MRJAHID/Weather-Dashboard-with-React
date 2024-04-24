b41643d4c3921e5427e71bd789bb0c3b74ec8828
MRJAHID <mrjahid350@gmail.com>
2024-04-25 00:05:58 +0600

    Populating UI With Data

--- src/components/Weather/WeatherBoard.jsx
+++ src/components/Weather/WeatherBoard.jsx
@@ -1,11 +1,13 @@
 import AddToFavourite from "./AddToFavourite.jsx";
 import WeatherHeadline from "./WeatherHeadline.jsx";
 import WeatherCondition from "./WeatherCondition.jsx";
-import {useWeather} from "../../hooks/index.js";
+import {useContext} from "react";
+import {WeatherContext} from "../../context/index.js";
 
 const WeatherBoard = () => {
-    const {error, loading, weather} = useWeather();
-    console.log(error, loading, weather);
+
+    // Get Data from WeatherContext
+    const {weather, loading} = useContext(WeatherContext);
 
     return (
         <section className="">
@@ -13,9 +15,17 @@ const WeatherBoard = () => {
                 <div
                     className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
                     <div className="grid md:grid-cols-2 gap-10 md:gap-6">
-                        <AddToFavourite/>
-                        <WeatherHeadline/>
-                        <WeatherCondition/>
+                        {/* Conditionally Loading State */}
+                        {
+                            loading.state ? (<p>{loading.message}</p>) : (
+                                <>
+                                    <AddToFavourite/>
+                                    <WeatherHeadline/>
+                                    <WeatherCondition/>
+                                </>
+                            )
+                        }
+
                     </div>
                 </div>
 
--- src/components/Weather/WeatherHeadline.jsx
+++ src/components/Weather/WeatherHeadline.jsx
@@ -1,20 +1,60 @@
 import CloudIcon from '../../assets/cloud.svg';
+import HazeIcon from '../../assets/haze.svg';
+import SnowIcon from '../../assets/icons/snow.svg';
+import SunnyIcon from '../../assets/icons/sunny.svg';
+import RainIcon from '../../assets/rainy.svg';
 import PinIcon from '../../assets/pin.svg';
+import ThunderIcon from '../../assets/thunder.svg';
+
+import {useContext} from "react";
+import {WeatherContext} from "../../context/index.js";
+import {getFormattedDate} from "../../utils/date-util.js";
 
 const WeatherHeadline = () => {
+    const {weather} = useContext(WeatherContext);
+    const {temperature, location, climate, time} = weather;
+
+    // * get Relative Icon About Weather
+    function getWeatherIcon(climate) {
+        switch (climate) {
+            case "Rain":
+                return RainIcon;
+            case "Clouds":
+                return CloudIcon;
+            case "Clear":
+                return SunnyIcon;
+            case "Snow":
+                return SnowIcon;
+            case "Thunder":
+                return ThunderIcon;
+            case "Fog":
+                return HazeIcon;
+            case "Haze":
+                return HazeIcon;
+            case "Mist":
+                return HazeIcon;
+
+            default:
+                return SunnyIcon;
+        }
+    }
+
+
     return (
         <div>
             <div className="max-md:flex items-center justify-between md:-mt-10">
-                <img src={CloudIcon} alt="cloud"/>
+                <img src={getWeatherIcon(climate)} alt="cloud"/>
                 <div className="max-md:flex items-center max-md:space-x-4">
-                    <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">16°</h1>
+                    <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">{Math.round(temperature)}°</h1>
                     <div className="flex items-center space-x-4 md:mb-4">
                         <img src={PinIcon} alt='PinIcon'/>
-                        <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+                        <h2 className="text-2xl lg:text-[50px]">{location}</h2>
                     </div>
                 </div>
             </div>
-            <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
+
+            {/* Use getFormattedDate Function for Format Date*/}
+            <p className="text-sm lg:text-lg">{getFormattedDate(time, "time", false)} - {getFormattedDate(time, "date", false)}</p>
         </div>
     );
 };
