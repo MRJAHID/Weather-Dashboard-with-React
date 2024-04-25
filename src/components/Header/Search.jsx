6848a9174fe52a3eb158889c4a32e481ca81eb6e
MRJAHID <mrjahid350@gmail.com>
2024-04-25 19:49:57 +0600

    useDebounce Custom hook for delay request

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -1,36 +1,38 @@
+import { useContext } from "react";
 import SearchIcon from "../../assets/search.svg";
-import {useContext, useState} from "react";
-import {LocationContext} from "../../context/index.js";
-import {getLocationByName} from "../../data/location-data.js";
+import { LocationContext } from "../../context";
+import { getLocationByName } from "../../data/location-data";
+import { useDebounce } from "../../hooks";
 
-const Search = () => {
-    const[ searchTerm, setSearchTerm ] = useState('');
-    const {setSelectedLocation} = useContext(LocationContext);
+function Search() {
+    const { setSelectedLocation } = useContext(LocationContext);
 
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        const fetchedLocation = getLocationByName(searchTerm);
+    const doSearch = useDebounce((term) => {
+        const fetchedLocation = getLocationByName(term);
         setSelectedLocation({ ...fetchedLocation });
-        console.log(fetchedLocation)
+    }, 500);
+
+    function handleChange(e) {
+        const value = e.target.value;
+        doSearch(value);
     }
 
-
     return (
-        <form action="#" onSubmit={handleSubmit}>
-            <div
-                className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
-
-                <input onChange={(e) => setSearchTerm(e.target.value)}
+        <form action="#">
+            <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
+                <input
                     className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
-                    type="search" placeholder="Search Location" required/>
-
+                    type="search"
+                    placeholder="Search Location"
+                    onChange={handleChange}
+                    required
+                />
                 <button type="submit">
-                    <img src={SearchIcon} alt='Search Icon'/>
+                    <img src={SearchIcon} alt="Search" />
                 </button>
             </div>
         </form>
     );
-};
+}
 
 export default Search;
